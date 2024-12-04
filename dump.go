1da91395d5369a0b86ae866f8618535d8899c926
Dreadyyyy <aboutegor8@gmail.com>
2024-12-05 02:26:36 +0300

    Added basic IO

--- dump.go
+++ dump.go
@@ -9,7 +9,7 @@ func dump(s []byte) (out string) {
 	for i := 0; i < len(s); i += 8 {
 		out += fmt.Sprintf("%08x:", i)
 		for _, b := range s[i:min(i+8, len(s))] {
-			out += fmt.Sprintf(" %2x", b)
+			out += fmt.Sprintf(" %02x", b)
 		}
 		out += "\n"
 	}
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println(dump([]byte("abcdefghijklmnopqrstuvwxyz")))
+	var err error
+
+	if len(os.Args) > 3 {
+		fmt.Fprintf(os.Stderr, "Usage: gox [infile[outfile]]\n")
+		os.Exit(1)
+	}
+
+	in, out := os.Stdin, os.Stdout
+
+	if len(os.Args) >= 2 {
+		in, err = os.Open(os.Args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	if len(os.Args) == 3 {
+		out, err = os.Create(os.Args[2])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	buf := bytes.NewBuffer(nil)
+	if _, err = io.Copy(buf, in); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(out, dump(buf.Bytes()))
 }
