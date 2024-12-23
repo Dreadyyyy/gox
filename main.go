cc326e236faa86729c7fb601fb07298a2e354436
Dreadyyyy <aboutegor8@gmail.com>
2024-12-23 21:30:26 +0300

    Added plain hex dump style and option to add offset

--- main.go
+++ main.go
@@ -11,36 +11,38 @@ const (
     gox [options] [infile[outfile]]
 Options:
     -h | -help  Show help
+    -ps|        Plain hex dump output style
     -v |        Verbose
     -c |        Format <cols> bytes per line
+    -o |        Add <offset> to the displayed file position
     `
-	COLS = "Invalid number of columns: max 256\n"
+	COLS = "Invalid number of columns: max 256"
 )
 
 func main() {
 	var err error
 
-	h := false
-	flag.BoolVar(&h, "h", false, "Show help")
-	flag.BoolVar(&h, "help", false, "Show help")
+	h := flag.Bool("help", false, "Show help")
+	ps := flag.Bool("ps", false, "Plain hex dump output style")
 	v := flag.Bool("v", false, "Verbose")
 	c := flag.Int("c", 8, "Format <cols> bytes per line")
+	o := flag.Int("o", 0, "Add offset")
 
 	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, USAGE)
+		fmt.Fprintln(os.Stderr, USAGE)
 	}
 
 	flag.Parse()
 
 	if *c > 256 {
-		fmt.Fprint(os.Stderr, COLS)
+		fmt.Fprintln(os.Stderr, COLS)
 		os.Exit(1)
 	}
 
 	args := flag.Args()
 
-	if len(args) > 2 || h {
-		fmt.Fprint(os.Stderr, USAGE)
+	if len(args) > 2 || *h {
+		fmt.Fprintln(os.Stderr, USAGE)
 		os.Exit(1)
 	}
 
@@ -62,7 +64,7 @@ func main() {
 		}
 	}
 
-	for l, err := range unixSeq(in, *v, *c) {
+	for l, err := range dumpSeq(in, *ps, *v, *c, *o) {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 			os.Exit(1)
